bff033b385c25d5f65c6c905d2fbf43242a84afa
F1low <aram8928365188@gmail.com>
2024-02-29 18:20:50 +0300

    fix and add new fam

--- docs/assets/dist/js/main.js
+++ docs/assets/dist/js/main.js
@@ -1,12 +1,11 @@
 function feedback(action, status) {
-    //
     let token = "7043261922:AAG-ImKOB9k6P6Gv8sV7Ge94_IBVIgiwXRw";/* :TODO NOTSECURITY REWERITE */
     //use this for testing
    //let chat_id = "190404167";
     //Aram ID
     //use this for production
     let chat_id = "1329475336";
-    let user_phone = prompt('Введите discord для контакта с вами');
+    let user_phone = prompt('Введите для Discord name контакта с вами');
     var msg = `${action} от ${user_phone}`;// from ${getCookie("@")}`;
     var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${msg}&parse_mode=html`;
   
